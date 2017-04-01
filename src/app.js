1c239c4a70020cd90ba32a431f243fb867258e5d
Yanelis <yaneliserranotaylor@gmail.com>
2017-04-01 11:03:14 +0200

    TheLast

--- src/app.js
+++ src/app.js
@@ -13,6 +13,26 @@ angular.module('app', ["ngRoute"]).
             templateUrl: 'views/create_recipe.html',
             controller: CreateRecipeController
             })
+            . when('/createdieta', {
+                templateUrl: 'views/create_dieta.html',
+                controller: DietaController
+            })
+            . when('/perfil', {
+                templateUrl: 'views/perfil.html',
+                controller: PerfilController
+            })
+            . when('/timeline', {
+                templateUrl: 'views/timeline.html',
+                controller: TimelineController
+            })
+            . when('/listrecipesuser', {
+                templateUrl: 'views/list_recepes-user.html',
+                controller: RecipesListUserController
+            })
+            . when('/listdietasuser', {
+                templateUrl: 'views/list_dieta_user',
+                controller: DietaListController
+            })
             .otherwise({ redirect: '/'});
 
        /* //mediante dos puntos (:) definimos un parámetro
