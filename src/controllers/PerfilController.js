e3dc5d199468c7b05202788218c87f8aabc8d93e
Yanelis <yaneliserranotaylor@gmail.com>
2017-04-18 18:44:44 +0200

    Perfil de usuario

--- src/controllers/PerfilController.js
+++ src/controllers/PerfilController.js
@@ -3,4 +3,10 @@
  */
 function PerfilController($scope, $http,  jwtHelper ) {
 
+    $scope.tokenPayload = jwtHelper.decodeToken(localStorage.getItem('token'));
+    var id =$scope.tokenPayload.id;
+
+    $http.get('http://localhost/backend-api/web/app_dev.php/api/user/'+id).success(function(data) {
+        $scope.user = data.usuario;
+    });
 }
